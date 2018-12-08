5738cc2a12feb4c67385c1b4de2be4cabc7d1877
Runer <>
2018-12-08 14:53:36 +0100

    Simple changes for tests

--- 01_hello-world/src/main/java/pl/sdacademy/App.java
+++ 01_hello-world/src/main/java/pl/sdacademy/App.java
@@ -5,6 +5,8 @@ package pl.sdacademy;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		//comment to test git
+		System.out.println("Hello World!"
+		);
 	}
 }
